1fd8780980400cd4fbf2ae1a91db092e750e7a73
Fabien Péan <fabien@pean.pro>
2025-02-23 17:53:06 +0000

    Cosmetic

--- include/comcept/details/macros.hpp
+++ include/comcept/details/macros.hpp
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 #pragma once
 
-#define COMCEPT_TRAITIFY_N(name)                                   \
+#define COMCEPT_TRAITIFY_N(name)                            \
     template<class... Args>                                 \
     struct name                                             \
     {                                                       \
@@ -9,7 +9,7 @@
         static constexpr bool value = std::name<T,Args...>; \
     }
 
-#define COMCEPT_TRAITIFY_3(name)                               \
+#define COMCEPT_TRAITIFY_3(name)                        \
     template<class U, class V>                          \
     struct name                                         \
     {                                                   \
@@ -17,7 +17,7 @@
         static constexpr bool value = std::name<T,U,V>; \
     }  
 
-#define COMCEPT_TRAITIFY_2(name)                               \
+#define COMCEPT_TRAITIFY_2(name)                        \
     template<class U>                                   \
     struct name                                         \
     {                                                   \
@@ -25,7 +25,7 @@
         static constexpr bool value = std::name<T,U>;   \
     }                                                 
 
-#define COMCEPT_TRAITIFY_1(name)                               \
+#define COMCEPT_TRAITIFY_1(name)                        \
     struct name                                         \
     {                                                   \
         template<class T>                               \
