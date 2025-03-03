b4b502030a04193cc5e0b99cbc8d8ebf520953fd
Fabien Péan <fabien@pean.pro>
2025-03-03 21:41:27 +0000

    Update test

--- tests/compose.cpp
+++ tests/compose.cpp
@@ -1,12 +1,10 @@
 #include <comcept/comcept.hpp>
 #include <type_traits>
 
-///====================================================================================================================
-/// Compile-time
-///====================================================================================================================
+namespace trt = comcept::trait;
 
-static_assert(    comcept::compose<std::is_integral>::value<int>);
-static_assert(not comcept::compose<std::is_integral>::value<float>);
+static_assert(    trt::compose<std::is_integral>::value<int>);
+static_assert(not trt::compose<std::is_integral>::value<float>);
 
-static_assert(    comcept::satisfy<  int,comcept::compose<std::is_integral>>);
-static_assert(not comcept::satisfy<float,comcept::compose<std::is_integral>>);
+static_assert(    comcept::satisfy<  int,trt::compose<std::is_integral>>);
+static_assert(not comcept::satisfy<float,trt::compose<std::is_integral>>);
